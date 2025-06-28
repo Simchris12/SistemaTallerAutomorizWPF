25a8111393757aacb42a09a46fe8ed81e301ba55
Simplechris12 <cristofercastillo12102000@gmai.com>
2025-06-28 12:02:35 -0400

    Creación de clase de conexión para los clientes

---
+++ SistemaTallerAutomorizWPF/Models/Connections.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SistemaTallerAutomorizWPF.Models
+{
+    internal class Connections
+    {
+        public static class Connection
+        {
+            public static String ConnectionString = "Data Source=localhost;Initial Catalog=MVVMLogindb;Integrated Security=True";
+
+            public static SqlConnection ObtainConnection()
+            {
+                return new SqlConnection(ConnectionString);
+            }
+        }
+    }
+}
